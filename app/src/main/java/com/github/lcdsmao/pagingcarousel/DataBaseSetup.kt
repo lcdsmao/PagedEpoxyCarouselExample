d1cac3682cc727a01db38163fcc3f80247fa1d80
Mao <lcdsmao@gmail.com>
2019-07-06 10:06:28 +0900

    Copy paging sample codes

---
+++ app/src/main/java/com/github/lcdsmao/pagingcarousel/DataBaseSetup.kt
@@ -0,0 +1,45 @@
+package com.github.lcdsmao.pagingcarousel
+
+import androidx.paging.DataSource
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(User::class), version = 1)
+abstract class PagingDatabase : RoomDatabase() {
+  abstract fun userDao(): UserDao
+}
+
+@Entity
+data class User(
+  @PrimaryKey
+  var uid: Int,
+
+  @ColumnInfo(name = "first_name")
+  var firstName: String = "first name $uid",
+
+  @ColumnInfo(name = "last_name")
+  var lastName: String = "last name $uid"
+)
+
+@Dao
+interface UserDao {
+  @get:Query("SELECT * FROM user")
+  val dataSource: DataSource.Factory<Int, User>
+
+  @get:Query("SELECT * FROM user")
+  val all: List<User>
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insertAll(users: List<User>)
+
+  @Delete
+  fun delete(users: List<User>)
+}
